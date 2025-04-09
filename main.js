497ef22ede9dc4829a91592c7f52f7cf2683116a
Alessandro Frisone <alefri2004@gmail.com>
2025-04-09 12:25:16 +0200

    FEAT: SNACK

---
+++ main.js
@@ -0,0 +1,110 @@
+// ---------------------------------------------------------------------------------------------------- //
+
+// 🏆 Code Question 1
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?  1
+
+// const hamburger = { name: "Cheese Burger", weight: 250 };
+// const secondBurger = hamburger;
+
+// secondBurger.name = 'Double Cheese Burger';
+// secondBurger.weight = 500;
+
+// console.log(hamburger.name); // Double Cheese Burger
+// console.log(secondBurger.name ); // Double Cheese Burger
+
+// ---------------------------------------------------------------------------------------------------- //
+
+// 🏆 Code Question 2
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3
+
+// const hamburger = { 
+// 	name: "Cheese Burger", 
+// 	weight: 250,
+// 	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+// };
+
+// const secondBurger = {...hamburger};
+// secondBurger.ingredients[0] = "Salad";
+
+// console.log(hamburger.ingredients[0]); // Salad
+// console.log(secondBurger.ingredients[0]); // Salad
+
+// ---------------------------------------------------------------------------------------------------- //
+
+// 🏆 Code Question 3
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 9 PERCHE NELL OGGETTO PRINCIPALE CI SONO 3 OGGETTI
+
+// const hamburger = { 
+// 	name: "Cheese Burger", 
+// 	weight: 250,
+// 	maker: {
+// 		name: "Anonymous Chef",
+// 		restaurant: {
+// 			name: "Hyur's Burgers",
+// 			address: "Main Street, 123",
+// 			isOpen: true,
+// 		},
+// 		age: 29
+// 	}
+// };
+
+// const secondBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
+
+// ---------------------------------------------------------------------------------------------------- //
+
+// 🏆 Code Question 4
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché?  SHALLOWCOPY PERCHE COPIA FUNZIONI
+// Qual è il metodo migliore per clonare l’oggetto restaurant, e perché? DEEPCOPY CON STRUCTURCLONE, PERCHE E UN OGGETTO COMPLESSO
+
+// const chef = {
+// 	name: "Chef Hyur",
+// 	age: 29,
+// 	makeBurger: (num = 1) => {
+// 		console.log(`Ecco ${num} hamburger per te!`);
+// 	},
+// }
+
+// const restaurant = {
+// 	name: "Hyur's Burgers",
+// 	address: {
+// 		street: 'Main Street',
+// 		number: 123,
+// 	},
+// 	openingDate: new Date(2025, 3, 11),
+// 	isOpen: false,
+// };
+
+// ---------------------------------------------------------------------------------------------------- //
+
+// 🎯 Code Question 5 (Bonus)
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3
+
+const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	maker: {
+		name: "Anonymous Chef",
+		restaurant: {
+			name: "Hyur's Burgers",
+			address: "Main Street, 123",
+			isOpen: true,
+		},
+		age: 29
+	}
+};
+
+const newRestaurant = {...hamburger.maker.restaurant};
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+const secondBurger = {...hamburger};
+secondBurger.maker.restaurant = newRestaurant;
+secondBurger.maker.name = "Chef Hyur";
+
+console.log(hamburger.maker.name); // Chef Hyur
+console.log(secondBurger.maker.name); // Chef Hyur
+console.log(hamburger.maker.restaurant.name); // Hyur's II
+console.log(secondBurger.maker.restaurant.name); // Hyur's II
